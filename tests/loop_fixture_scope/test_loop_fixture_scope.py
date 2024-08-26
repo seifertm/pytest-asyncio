33e96f4b18c9860c2ca5e80d5cf34b5d2c89dab4
Michael Seifert <m.seifert@digitalernachschub.de>
2024-08-26 08:28:21 +0200

    test!: Remove tests using a fixture that depends on event_loop.

--- tests/loop_fixture_scope/test_loop_fixture_scope.py
+++ tests/loop_fixture_scope/test_loop_fixture_scope.py
@@ -10,8 +10,3 @@ async def test_for_custom_loop():
     """This test should be executed using the custom loop."""
     await asyncio.sleep(0.01)
     assert type(asyncio.get_event_loop()).__name__ == "CustomSelectorLoop"
-
-
-@pytest.mark.asyncio
-async def test_dependent_fixture(dependent_fixture):
-    await asyncio.sleep(0.1)
